3df45d484aabe25ad689161ca5c00ff1f6067966
kzkvaan <kzkvaar@gmail.com>
2025-02-12 17:50:07 +0500

    Задания 2 и 3 лабораторных работ

---
+++ ООП Лаба 2/task1.py
@@ -0,0 +1,39 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+class Book:
+
+    def __init__(self, id_: int,name: str, pages: int):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+    def __str__(self) -> str:
+
+        #self.name = name
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+
+
+        return f"Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})" # ИЛИ name='{self.name}'
+
+if __name__ == '__main__':
+    # Инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+
+    for book in list_books:
+        print(book)
+
+    print(list_books)
